5cf3615563c528bdedc2398903250c97a48da518
darkaforest <wrcxblt@163.com>
2019-03-28 23:42:28 +0800

    init commit

---
+++ src/main/java/com/sduwh/liutao/searchengine/contorller/IndexController.java
@@ -0,0 +1,16 @@
+package com.sduwh.liutao.searchengine.contorller;
+
+/**
+ * Created by IntelliJ IDEA 2018.1.5 (Ultimate Edition)
+ * JRE: 1.8.0_172-release-1136-b39 x86_64
+ * JVM: OpenJDK 64-Bit Server VM by JetBrains s.r.o
+ * SYS: macOS Mojave 10.14.4
+ *
+ * @author darkaforest
+ * @date 2019/3/28 22:43
+ */
+public class IndexController {
+
+
+
+}
